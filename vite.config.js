4107ac0fd3af67cb0e2fe912ee5decea20bca801
32vike <vike1033456@gmail.com>
2025-08-31 17:05:01 +0530

    Fix GitHub Pages blank screen

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/crypto-website/', // 👈 Repo name (important for GitHub Pages)
+  base: '/crypto-website/',  // 👈 This fixes GitHub Pages blank screen
 })
